3de407734851ecdd97fc6f3927b4183ece3f3c45
Jonathan <alayj3665@gmail.com>
2026-01-20 16:55:37 -0500

    Fix database config and load initial data

--- Proyecto/build.sh
+++ Proyecto/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+set -o errexit
+
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py loaddata registro_utf8.json || true
-
+python manage.py loaddata fixtures/users.json || true
+python manage.py loaddata fixtures/registro_utf8.json || true
